3b93a176623a6687c7ef1af3d12a0d9a5c962201
Rajveer Chouhan <rajveer@cloudthat.com>
2025-04-15 10:50:28 +0530

    ec2 module

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,25 @@
+variable "ami" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID to launch the instance in"
+  type        = string
+}
+
+variable "key_name" {
+  description = "Key pair name for SSH access"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for the instance"
+  type        = string
+}
