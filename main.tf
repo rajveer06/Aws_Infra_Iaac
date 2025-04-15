8ebf881087c696ed3e4392c35b1ca9fe8ac58f21
Rajveer Chouhan <rajveer@cloudthat.com>
2025-04-15 13:17:47 +0530

    removing source from backend

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "s3" {
-    source = "./modules/s3"
     bucket         = "terraform-state-myproject-20250415"
     key            = "terraform/terraform.tfstate"
     encrypt        = true
